6fd0b076c274680fb55e7c5048844d22e9c036fb
cceer <sisir.sital@gmail.com>
2023-01-24 14:10:07 +0545

    initial commit copy brand dealer

--- next.config.js
+++ next.config.js
@@ -8,12 +8,7 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    env: {
-        NEXT_PUBLIC_STRIPE_SECRET_KEY:
-            'sk_test_51Hp8gKEhskqVAwrw1vcbenMyKucfDgRngHp5dDA6psNscmYE72N6lgZ2YEHtENKVOUivutURS750ifrMfxVN0SCX00RTiQLTf3',
-        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-            'pk_test_51Hp8gKEhskqVAwrw3FtKoOqoZ8rbrWSerS05PT0yBf0Pk3MrBPiogF57jwQLkS6Yf4mgTWXakqwsdsbj9lgG1KxL00szz0DI40',
-    },
+    env: {},
 };
 
 module.exports = nextConfig;
